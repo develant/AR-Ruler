3c5b545d8c63e0fe99cfa91077264c4eb4ccd787
Antons Aleksandrovs <aleksandrovs.antons@gmail.com>
2018-02-07 11:30:32 +0200

    Add functionality to measure the distance in AR, to create 3D text for displaying distance length and to delete previous points.

--- AR Ruler/ViewController.swift
+++ AR Ruler/ViewController.swift
@@ -12,13 +12,15 @@ import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
 
+    var dotNodes = [SCNNode]()
+    var textNode = SCNNode()
+    
     @IBOutlet var sceneView: ARSCNView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         sceneView.delegate = self
-        
         sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints]
     }
     
@@ -37,16 +39,21 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
+        if dotNodes.count >= 2 {
+            for dot in dotNodes {
+                dot.removeFromParentNode()
+            }
+            dotNodes = []
+            textNode.removeFromParentNode()
+        }
+        
         if let touchLocation = touches.first?.location(in: sceneView) {
             let hitTestResults = sceneView.hitTest(touchLocation, types: .featurePoint)
             
-         
             if let hitResult = hitTestResults.first {
                 addDot(at: hitResult)
             }
-            
         }
-        
     }
 
     func addDot(at hitResult: ARHitTestResult) {
@@ -63,6 +70,35 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         dotNode.position = SCNVector3(column.x, column.y, column.z)
         
         sceneView.scene.rootNode.addChildNode(dotNode)
+        dotNodes.append(dotNode)
+        
+        if dotNodes.count >= 2 {
+            calculate()
+        }
     }
     
+    func calculate() {
+        let start = dotNodes[0]
+        let end = dotNodes[1]
+        
+        let a = end.position.x - start.position.x
+        let b = end.position.y - start.position.y
+        let c = end.position.z - start.position.z
+        
+        let distance = sqrt(pow(a, 2) + pow(b, 2) + pow(c, 2))
+        
+        updateText(text: "\(distance) m", atPosition: end.position)
+    }
+    
+    func updateText(text: String, atPosition position: SCNVector3) {
+        
+        let textGeometry = SCNText(string: text, extrusionDepth: 1.0)
+        textGeometry.firstMaterial?.diffuse.contents = UIColor.red
+        
+        textNode.geometry = textGeometry
+        textNode.position = SCNVector3(position.x, position.y + 0.01, position.z)
+        textNode.scale = SCNVector3(0.01, 0.01, 0.01)
+        
+        sceneView.scene.rootNode.addChildNode(textNode)
+    }
 }
